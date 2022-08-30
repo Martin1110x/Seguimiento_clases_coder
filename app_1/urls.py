c819c74385a453ee721f108cc6fa3341799992a9
Martín <martinrega20@gmail.com>
2022-08-30 18:31:52 -0300

    solucionado_error_pathing

--- app_1/urls.py
+++ app_1/urls.py
@@ -4,10 +4,10 @@ from app_1.views import *
 
 urlpatterns = [
     path("curso/",curso),
-    path("", inicio),
-    path("cursos/", cursos),
-    path("estudiantes/", estudiantes),
-    path("profesores/", profesores),
-    path("entregables/", entregables),
+    path("", inicio, name = "inicio"),
+    path("cursos/", cursos, name = "cursos"),
+    path("estudiantes/", estudiantes, name = "estudiantes"),
+    path("profesores/", profesores, name = "profesores"),
+    path("entregables/", entregables, name = "entregables"),
 
 ]
