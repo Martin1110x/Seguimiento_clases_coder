4bee609160f5a06a9c8b9bc110e8009e393ef4dc
Martín <martinrega20@gmail.com>
2022-08-31 12:04:14 -0300

    configure_el_admin

--- app_1/models.py
+++ app_1/models.py
@@ -6,10 +6,17 @@ class Curso(models.Model):
     nombre = models.CharField(max_length=50)
     comision = models.IntegerField()
 
+    def __str__(self) -> str:
+        return f"{self.nombre} {self.comision}"
+
 class Estudiante(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     email = models.EmailField() 
+
+    def __str__(self) -> str:
+        return f"{self.nombre} {self.apellido}"
+
 
 class Profesor(models.Model):
     nombre = models.CharField(max_length=50)
@@ -17,7 +24,14 @@ class Profesor(models.Model):
     email = models.EmailField() 
     profesion = models.CharField(max_length=50)
 
+    def __str__(self) -> str:
+        return f"{self.nombre} {self.apellido}"
+
+
 class Entregable(models.Model):
     nombre = models.CharField(max_length=50)
     fecha_entrega = models.DateField()
     entregado = models.BooleanField()
+
+    def __str__(self) -> str:
+        return f"{self.nombre} {self.entregado}"
